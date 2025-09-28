cbea449ce855ff5fea592ff89209cd352ddf172f
Wtagnan <rivalinbudx795751937@gmail.com>
2025-09-28 19:10:03 +0300

    Added utils.py with helper functions

---
+++ utils.py
@@ -0,0 +1,6 @@
+# Вспомогательные функции для Horizon Forge Module
+
+def helper_function(config):
+    print("Вспомогательная функция запущена")
+    for key, value in config.items():
+        print(f"{key}: {value}")
